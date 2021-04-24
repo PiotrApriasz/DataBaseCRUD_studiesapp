4c17a09596c70603b3ef9e341ea7cea005ebe292
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-04-24 19:14:54 +0200

    Add variables to the class

--- DataBaseManager/DBConnect.cs
+++ DataBaseManager/DBConnect.cs
@@ -4,6 +4,33 @@ namespace DataBaseManager
 {
     public class DBConnect
     {
-        
+        #region Variables
+
+        /// <summary>
+        /// Will be used to open a connection to the database
+        /// </summary>
+        private MySqlConnection _connection;
+
+        /// <summary>
+        /// Indicates where our server is hosted. Here it is localhost
+        /// </summary>
+        private string _server;
+
+        /// <summary>
+        /// Will be used to store database name to use in program
+        /// </summary>
+        private string _database;
+
+        /// <summary>
+        /// MySQL username
+        /// </summary>
+        private string _uid;
+
+        /// <summary>
+        /// MySQL password
+        /// </summary>
+        private string _password;
+
+        #endregion
     }
 }
